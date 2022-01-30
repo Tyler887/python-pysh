eb6d8f324a907379186873b72af15552dd943f4a
Tyler887 <tylermageeshields@gmail.com>
2022-01-30 16:41:27 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -2,14 +2,15 @@ print("Pyshell  Copyright (C) 2022  Tyler887 Github")
 print("This program comes with ABSOLUTELY NO WARRANTY.")
 print("This is free software, and you are welcome to redistribute it")
 print("under certain conditions.")
-
+print("")
 import os
 import pip
 import platform
 while True:
-  command = input(" > ")
+  cd = os.getcwd()
+  command = input(cd + "> ")
   if command == "print" or command == "print ":
     print("pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation")
   if command.startswith("print "):
-    echo = dict.fromkeys(map(ord, 'print '), None)
+    echo = command('print ', '', 1)
     print(echo)
