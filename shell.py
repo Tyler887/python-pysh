321582d7f10a1f14f503e4227e9b5ee0f2a2ca33
Tyler887 <tylermageeshields@gmail.com>
2022-01-30 19:56:05 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -16,15 +16,15 @@ while True:
   cd = os.getcwd()
   command = input("PYSH " + cd + " > ")
   if command == "print" or command == "print ":
-    print(bcolors.FAIL + "pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation" + bcolors.RESET)
+    print(f"{bcolors.FAIL}pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation{bcolors.RESET}")
   elif command == "exit":
-    exitconf = input(bcolors.WARNING + "Are you sure you want to exit PyShell? (y/n)\nanswer: " + bcolors.RESET)
+    exitconf = input(f("{bcolors.WARNING}Are you sure you want to exit PyShell? (y/n){bcolors.RESET}\nanswer: ")
     if exitconf == "y":
       exit(0)
   elif command == "commands":
      print("Commands: print, commands, exit\nTo add more commands, submit a pull request at https://github.com/Tyler887/python-pysh.")
   elif not command.startswith("print "):
-    print(bcolors.FAIL + "Command not found or misused. Run `commands' for a list of commands.\nHINT: This error counts as a bug. If you tried to use\necho, you should change it to print." + bcolors.RESET)
+    print(f"{bcolors.FAIL}Command not found or misused. Run `commands' for a list of commands.\nHINT: This error counts as a bug. If you tried to use\necho, you should change it to print.{bcolors.RESET}")
   if command.startswith("print "):
     echo = command.replace('print ', '', 1)
     print(echo)
