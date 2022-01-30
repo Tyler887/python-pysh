55616fbe88866a4c0864c39154c1ca8af625df1d
Tyler887 <tylermageeshields@gmail.com>
2022-01-30 16:23:07 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -7,7 +7,7 @@ import os
 import pip
 import platform
 while True:
-  print(os.getcwd end='')
+  print(os.getcwd() end='')
   command = input(" > ")
   if command = "print" or command = "print ":
     print("pysh aborted as it found a bug!\nbug: cannot use only `print', must set input for operation")
