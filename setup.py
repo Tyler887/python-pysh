9d6d412534b9e416bac63125246aab2adace121e
Tyler887 <tylermageeshields@gmail.com>
2022-01-31 21:57:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 print("Installing Nessecary Dependencies...")
 os.system('python3 -m pip install art')
+os.system('python3 -m pip install setup')
 from art import *
 import platform
+import setup
 setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
