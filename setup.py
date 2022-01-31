803690cd871e8fe2fba4a05e272cbff1ebcd792b
Tyler887 <tylermageeshields@gmail.com>
2022-01-31 21:54:09 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 try:
   import requests
 except ImportError:
-  print "Trying to Install required module: requests\n"
+  print("Trying to Install required module: requests\n")
   os.system('python -m pip install requests')
 # -- above lines try to install requests module if not present
 # -- if all went well, import required module again ( for global access)
