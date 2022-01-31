25ef02a4e7723b377b432bdf6621e79f75920e37
Tyler887 <tylermageeshields@gmail.com>
2022-01-31 21:35:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup # Need this to handle modules
-os.system('python3 -m pip install art')
 import py2exe 
 from os import *
 import pip
+os.system('python3 -m pip install art')
 from art import *
 import platform
 setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
