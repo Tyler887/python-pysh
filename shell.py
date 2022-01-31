55d4b29d0baeaaf72d0d0de6041349e20f9aa6dc
Tyler887 <tylermageeshields@gmail.com>
2022-01-31 15:45:15 +0000

    notepad++

--- shell.py
+++ shell.py
@@ -1,30 +1,27 @@
-print("Pyshell  Copyright (C) 2022  Tyler887 Github")
-print("This program comes with ABSOLUTELY NO WARRANTY.")
-print("This is free software, and you are welcome to redistribute it")
-print("under certain conditions.")
+print("PyShell v1.0 - Open Source")
 print("")
 import os
 import pip
 import platform
-class bcolors:
-    OK = '\033[92m' #GREEN
-    WARNING = '\033[93m' #YELLOW
-    FAIL = '\033[91m' #RED
-    RESET = '\033[0m' #RESET COLOR
+
 
 while True:
   cd = os.getcwd()
   command = input("PYSH " + cd + " > ")
   if command == "print" or command == "print ":
-    print(f"{bcolors.FAIL}pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation{bcolors.RESET}")
+    print("pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation")
   elif command == "exit":
-    exitconf = input(f"{bcolors.WARNING}Are you sure you want to exit PyShell? (y/n){bcolors.RESET}\nanswer: ")
+    exitconf = input("Are you sure you want to exit PyShell? (y/n)\nanswer: ")
     if exitconf == "y":
       exit(0)
   elif command == "commands":
-     print("Commands: print, commands, exit\nTo add more commands, submit a pull request at https://github.com/Tyler887/python-pysh.")
+     print("Commands: print, commands, exit, python-license, version\nTo add more commands, submit a pull request at https://github.com/Tyler887/python-pysh.")
+  elif command == "python-license":
+    license()
+  elif command == "version":
+    print("1.0 Open Source")
   elif not command.startswith("print "):
-    print(f"{bcolors.FAIL}Command not found or misused. Run `commands' for a list of commands.\nHINT: This error counts as a bug. If you tried to use\necho, you should change it to print.{bcolors.RESET}")
+    print("Command not found or misused. Run `commands' for a list of commands.\nHINT: This error counts as a bug. No commands\nhave aliases.")
   if command.startswith("print "):
     echo = command.replace('print ', '', 1)
     print(echo)
