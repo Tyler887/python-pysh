3689454e83fb0306403b293bad807b59b6b6dd75
Tyler887 <tylermageeshields@gmail.com>
2022-02-01 15:24:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ os.system('python3 -m pip install art')
 from art import *
 from platform import *
 from py2exe import *
-py2exe.setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
+setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
