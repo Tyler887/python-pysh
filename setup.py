6856844479368ce5961344ca9d057f7a3f286b44
Tyler887 <tylermageeshields@gmail.com>
2022-02-01 15:22:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ print("Installing Nessecary Dependencies...")
 os.system('python3 -m pip install art')
 from art import *
 from platform import *
-from ps2exe import *
-setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
+from py2exe import *
+py2exe.setup(console=['./shell.py']) # Calls setup function to indicate that we're dealing with a single console application
