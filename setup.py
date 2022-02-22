7c7241d61663d198dae30619f3411507395df16a
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 19:34:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import *
 from colorama import *
 from art import *
 from sys import *
-from sysconfigimport *
+from sysconfig import *
 from platform import *
 import msvcrt as m
 setup(console=['shell.py']) # Calls setup function to indicate that we're dealing with a single console application
