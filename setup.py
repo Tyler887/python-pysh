217c886bd63cc6dae1b75be9b5674ada092335cb
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 19:28:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup # Need this to handle modules
 import py2exe 
-import time, os, colorama, art, sys, sysconfig, platform # We have to import all modules used in our program
+from time, os, colorama, art, sys, sysconfig, platform import * # We have to import all modules used in our program
 import msvcrt as m
 setup(console=['shell.py']) # Calls setup function to indicate that we're dealing with a single console application
