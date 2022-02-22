4c17df7df3c93db9318cee3c26d409aa99a9e75c
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 19:40:22 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
-from distutils.core import setup # Need this to handle modules
-import py2exe 
-from time import *
-from os import *
-from colorama import *
-from art import *
-from sys import *
-from sysconfig import *
-from platform import *
-import msvcrt as m
-setup(console=['shell.py']) # Calls setup function to indicate that we're dealing with a single console application
+from distutils.core import setup
+import py2exe
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    console = [{'script': "shell.py"}],
+    zipfile = None,
+)
