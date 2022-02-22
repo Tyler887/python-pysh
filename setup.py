a0b0fdd95a2ae14af22b7f39cfbe08865b927375
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 20:09:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import py2exe
 class Target:
     def __init__(self, **kw):
         self.__dict__.update(kw)
-        self.version = "1.0.0.0"
+        self.version = "1.1"
         self.company_name = "Tyler Eight-Eight-Seven"
         self.copyright = "Copyright (c) 2022 Tyler887, All Rights Reserved"
         self.name = "My Program"
