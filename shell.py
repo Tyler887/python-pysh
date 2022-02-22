92c200ac72d8c7e88d2d1e3e977bfafb0dd29582
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 19:18:59 +0000

    Update shell.py

--- shell.py
+++ shell.py
@@ -8,19 +8,26 @@
   # It is a free and open source shell,
   # which can also execute commands outside
   # itself.
-################# CODE BELOW #################
+################# CODE BELOW #############
 import os
 from os import *
 import time
 # thanks stack overflow
 os.system('cls' if os.name=='nt' else 'clear')
-print("Loading...")
+
+
+print("Loading PySH 1.1...")
+print("------------------------------------------------------------------")
+print("Upgrading pip...")
 os.system('python3 -m pip install -U pip')
+print("------------------------------------------------------------------")
+print("Installing dependencies...")
 os.system('python3 -m pip install -U art colorama --exists-action s')
-from art import * 
+print("Import art module")
+from art import *
+print("Import colorama module")
 from colorama import *
-
-
+print("Launching PySH...")
 
 
 # thanks stack overflow
@@ -41,8 +48,7 @@ if platform.system() == "Windows":
 import tkinter as tk
 while True:
   cd = os.getcwd()
-  command = input(Back.WHITE + Fore.BLACK + " PYSH " + Style.RESET_ALL + " " + cd + " > " + Fore.CYAN)
-  print(Style.RESET_ALL, end="")
+  command = input(Back.WHITE + Fore.BLACK + " PYSH " + Style.RESET_ALL + " " + cd + " > ")
   if command == "print" or command == "print ":
     tprint(errortitle)
     print("pysh aborted as it found a bug in your command!\nbug: cannot use only `print', must set input for operation")
@@ -52,7 +58,7 @@ while True:
     if exitconf == "y":
       exit(0)
   elif command == "commands":
-     print("Commands: print, commands, exit, cls, version, file\nTo add more commands, submit a pull request at https://github.com/Tyler887/python-pysh.")
+     print("Commands: print, commands, exit, cls, version, info\nPlanned: update\nTo add more commands, submit a pull request at https://github.com/Tyler887/python-pysh.\nCommands not bundled can also be executed in PySH (on Windows, WPS is used).")
   elif command == "file":
     tprint(manpagetitle)
     print(Fore.RED + "This command has been removed. Documentation for this command will not be updated." + Style.RESET_ALL)
@@ -74,9 +80,6 @@ while True:
   elif command == "version":
     print("1.1 Open Source")
     print("more info: run `info'")
-  elif command == "license":
-    license = tk.Label(text=open(cd2 + "/LICENSE", "r").read())
-    license.pack()
   elif command == "cls":
     # thanks stack overflow
     os.system('cls' if os.name=='nt' else 'clear')
@@ -107,3 +110,4 @@ while True:
     print("This command has been removed because it did not work in any PyShell version." + Style.RESET_ALL)
     print(Fore.BLUE + "Notice: There is another feature similar to the FILE command, external command support.\nConsider using external commands." + Style.RESET_ALL)
   
+# End script
