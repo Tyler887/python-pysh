f224ce3b9e43355b577535ffe65bca9e16b33e02
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 20:09:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from distutils.core import setup
 import py2exe
 
+class Target:
+    def __init__(self, **kw):
+        self.__dict__.update(kw)
+        self.version = "1.0.0.0"
+        self.company_name = "Tyler Eight-Eight-Seven"
+        self.copyright = "Copyright (c) 2022 Tyler887, All Rights Reserved"
+        self.name = "My Program"
+
+target = Target(
+    description = "The PyShell project is an open source project to make advanced users more productive.",
+    script = "Main.py",
+    dest_base = "PyShell")
 setup(
-    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
-    console = [{'script': "shell.py"}],
+    options = {'py2exe': {'bundle_files': 1,
+                          'compressed': True}},
     zipfile = None,
-    dest_base = "PyShell for Windows",
+    console = [target]
 )
