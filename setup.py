7be80ed1a9009d2024596719b6a440b33deab559
Tyler887 <tylermageeshields@gmail.com>
2022-02-22 20:03:45 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
     console = [{'script': "shell.py"}],
     zipfile = None,
-    "dest_base" : "PyShell for Windows",
+    dest_base = "PyShell for Windows",
 )
